1cfa13670967d2e85b6bc6fc4abc08de22846529
StephYa7 <1000p@mail.ru>
2024-02-16 22:44:45 +0700

    Initial commit

---
+++ src/main/java/st/firstsecurityspring/FirstSecuritySpringApplication.java
@@ -0,0 +1,13 @@
+package st.firstsecurityspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSecuritySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSecuritySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/st/firstsecurityspring/FirstSecuritySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package st.firstsecurityspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSecuritySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
