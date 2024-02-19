c9e84370ee96fcb7d0e66b8e97e887ae3399e320
StephYa7 <1000p@mail.ru>
2024-02-19 23:47:31 +0700

    change method maprolestoauthorities

--- src/main/java/st/firstsecurityspring/security/CustomUserDetailsService.java
+++ src/main/java/st/firstsecurityspring/security/CustomUserDetailsService.java
@@ -29,11 +29,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 
     /**
-     *  Используется для загрузки информации о пользователе по его электронной почте.
-     *  В данном методе происходит поиск пользователя в репозитории по электронной почте,
-     *  и если пользователь найден, создается объект UserDetails,
-     *  содержащий информацию о пользователе (электронная почта, пароль и роли).
-     *  В случае, если пользователь не найден, генерируется исключение UsernameNotFoundException
+     * Используется для загрузки информации о пользователе по его электронной почте.
+     * В данном методе происходит поиск пользователя в репозитории по электронной почте,
+     * и если пользователь найден, создается объект UserDetails,
+     * содержащий информацию о пользователе (электронная почта, пароль и роли).
+     * В случае, если пользователь не найден, генерируется исключение UsernameNotFoundException
      *
      * @param email
      */
@@ -44,7 +44,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         if (user != null) {
             return new org.springframework.security.core.userdetails.User(user.getEmail(),
                     user.getPassword(),
-                    mapRolesToAuthorities(Collections.singleton(user.getRole())));
+                    mapRolesToAuthorities(user.getRole()));
         } else {
             throw new UsernameNotFoundException("Invalid username or password.");
         }
@@ -54,11 +54,9 @@ public class CustomUserDetailsService implements UserDetailsService {
      * Используется для преобразования ролей пользователя в объекты SimpleGrantedAuthority,
      * которые представляют разрешения пользователя в Spring Security
      *
-     * @param roles the roles list
+     * @param role the role from user
      */
-    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
-        return roles.stream()
-                .map(role -> new SimpleGrantedAuthority(role.name()))
-                .toList();
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Role role) {
+        return Collections.singleton(new SimpleGrantedAuthority(role.toString()));
     }
 }
